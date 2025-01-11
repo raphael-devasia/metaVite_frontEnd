ef9c4cf7dc4f6e7199305de9a67963ee0642460e
raphael-devasia <jinomaliyekkal@Jinos-MacBook-Air.local>
2025-01-11 12:13:59 +0530

    path update from http to https

--- src/app/features/carrier/services/carrier.service.ts
+++ src/app/features/carrier/services/carrier.service.ts
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs';
 export class CarrierService {
   // private baseUrl = 'http://localhost:4000/';
   private baseUrl =
-    'https://a4624d01d367b4e51ae51cbe6f066c92-452922102.eu-north-1.elb.amazonaws.com:4000/';
+    'https://a4624d01d367b4e51ae51cbe6f066c92-452922102.eu-north-1.elb.amazonaws.com/';
 
   http = inject(HttpClient);
   constructor() {}
