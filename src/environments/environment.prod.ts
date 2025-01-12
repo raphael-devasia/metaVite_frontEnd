07b762c5c3467aafd379c1851224b15590503e72
raphael-devasia <jinomaliyekkal@Jinos-MacBook-Air.local>
2025-01-12 22:34:18 +0530

    http to wss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: true,
+//   SOCKET_ENDPOINT: 'https://metavite.ddns.net/socketiochat/',
+//   SOCKET_ENDPOINT2: 'https://metavite.ddns.net/socketio/',
+// };
 export const environment = {
   production: true,
-  SOCKET_ENDPOINT: 'https://metavite.ddns.net/socketiochat',
-  SOCKET_ENDPOINT2: 'https://metavite.ddns.net/socketio',
+  // Use the appropriate WebSocket URLs for each service
+  SOCKET_ENDPOINT: 'wss://metavite.ddns.net/socketiochat/', // For socketiochat service
+  SOCKET_ENDPOINT2: 'wss://metavite.ddns.net/socketio/', // For socketio service
 };
