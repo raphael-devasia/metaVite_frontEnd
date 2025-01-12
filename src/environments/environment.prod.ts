b7cdad26cf0e662b3c1e1cf0f47e2cc5f9325ce2
raphael-devasia <jinomaliyekkal@Jinos-MacBook-Air.local>
2025-01-12 22:14:48 +0530

    socketio endpoint update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SOCKET_ENDPOINT: 'https://metavite.ddns.net:3000',
-  SOCKET_ENDPOINT2: 'https://metavite.ddns.net:5001',
+  SOCKET_ENDPOINT: 'https://metavite.ddns.net/socketiochat',
+  SOCKET_ENDPOINT2: 'https://metavite.ddns.net/socketio',
 };
